a1b79f165f0cc791ef7d5186283ee6167154f864
GustShad <gf588026@gmail.com>
2025-06-17 20:25:26 -0300

    SuperTrunfo.c

---
+++ SuperTrunfo.c
@@ -0,0 +1,15 @@
+// Verificação de idade para votação
+if (idade >= 16) {
+    printf("Você pode votar!");
+} else {
+    printf("Você ainda não pode votar.");
+}
+
+// Comparação de preços
+if (preco1 < preco2) {
+    printf("O produto 1 é mais barato.");
+} else if (preco1 > preco2) {
+    printf("O produto 2 é mais barato.");
+} else {
+    printf("Os produtos têm o mesmo preço.");
+}
